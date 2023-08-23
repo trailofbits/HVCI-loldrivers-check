6f35fdbaa8a2795ec79cea77b0049753cd61849a
Yarden Shafir <yardenshafir2@gmail.com>
2023-08-23 15:19:01 -0400

    Update script and add counts

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # HVCI-loldrivers-check
-Checks to see which drivers from loldrivers.io are not blocked by a certain HVCI blocklist.
+Checks to see which drivers from loldrivers.io are not blocked by the current HVCI blocklist on the system.
 
 ### Requirements
 * Powershell 6.0+
 
 ### Usage
 ```
-.\check_allowed_drivers.ps1 -loldrivers_path <path to loldrivers JSON> -policy_path <path to policy XML>
+.\check_allowed_drivers.ps1
 ```
-* The loldrivers JSON file can be found here: https://www.loldrivers.io/
-* To recover the HVCI driver policy you can use this script: https://gist.github.com/mattifestation/92e545bf1ee5b68eeb71d254cec2f78e
+* The script will automatically pull the loldrivers JSON file which can be found here: https://www.loldrivers.io/
+* The script will automatically parse the HVCI driver policy.
+  If you'd like to extract the list yourself, you can use this script: https://gist.github.com/mattifestation/92e545bf1ee5b68eeb71d254cec2f78e
 
 ### Output
 ```
@@ -18,6 +19,7 @@ MD5: <hash> SHA1: <hash> SHA256: <hash>
 MD5: <hash> SHA1: <hash> SHA256: <hash>
 ...
 
+(optional)
 Maybe allowed:
 MD5: <hash> SHA1: <hash> SHA256: <hash>
 MD5: <hash> SHA1: <hash> SHA256: <hash>
--- check_allowed_drivers.ps1
+++ check_allowed_drivers.ps1
@@ -1,23 +1,47 @@
-#Requires -Version 6.0
-param (
-        [Parameter(Position = 0, Mandatory)]
-        [String]
-        [ValidateScript({ [IO.File]::Exists((Resolve-Path $_).Path) })]
-        $loldrivers_path,
+<#
+.SYNOPSIS
 
-        [Parameter(Position = 1, Mandatory)]
-        [String]
-        [ValidateScript({ [IO.File]::Exists((Resolve-Path $_).Path) })]
-        $policy_path
-)
+Compares the HVCI block list on the current system against the list of
+vulnerable and malicious drivers from loldrivers.io
 
-$loldrivers = Get-Content $loldrivers_path | ConvertFrom-Json -AsHashtable
-[xml]$policy = Get-Content $policy_path
+Company: Trail of Bits
+Author: Michael Lin
+Contributors: Yarden Shafir
+License: Apache 2
+
+.DESCRIPTION
+
+check_allowed_drivers.ps1 reports the drivers from loldrivers.io which are allowed
+by the current HVCI block list on the system.
+
+Note: drivers which are allowed by the HVCI block list might still not load
+      on a system due to other reasons such as architecture incompatibility,
+      incorrect signature or EDR software.
+
+.OUTPUTS
+
+Outputs a list of drivers not blocked by the HVCI policy.
+Potentially outputs a (short) list of drivers which may be allowed (see readme for details).
+#>
+
+$loldrivers = Invoke-WebRequest -Uri https://www.loldrivers.io/api/drivers.json | ConvertFrom-Json -AsHashTable
+$cipolicypath = $env:TEMP + '\CIPolicyParser.ps1'
+$recovered_policy_path = $env:TEMP + '\recovered_policy.xml'
+Invoke-WebRequest -Uri https://gist.githubusercontent.com/mattifestation/92e545bf1ee5b68eeb71d254cec2f78e/raw/a9b55d31075f91b467a8a37b9d8b2d84a0aa856b/CIPolicyParser.ps1 -OutFile $cipolicypath
+
+# CIPolicyParser is imcompatible with .NET Core so must be run in old powershell through Invoke-Command.
+# save result of Invoke-Command into variable so it won't write output to console
+$v = Invoke-Command -ScriptBlock { powershell {Import-Module ($env:TEMP + '\CIPolicyParser.ps1'); ConvertTo-CIPolicy -BinaryFilePath C:\Windows\System32\CodeIntegrity\driversipolicy.p7b -XmlFilePath ($env:TEMP + '\recovered_policy.xml')} }
+[xml]$policy = Get-Content $recovered_policy_path
+
+Remove-Item -Path $recovered_policy_path
+Remove-Item -Path $cipolicypath
 
 $file_rules = $policy.SiPolicy.FileRules
 $signers = $policy.SiPolicy.Signers.Signer
 $allowed = New-Object System.Collections.Generic.HashSet[string]
 $maybe_allowed = New-Object System.Collections.Generic.HashSet[string]
+$not_allowed = New-Object System.Collections.Generic.HashSet[string]
 
 function hasBlockedHash($driver){
     foreach($hash in $file_rules.Deny.Hash){
@@ -48,32 +72,42 @@ function hasBlockedSigner($driver){
                 return $true
             }
         }
-    }
-    if($file_attrib){
-        $maybe_allowed.Add(("MD5:{0} SHA1:{1} SHA256:{2}" -f $driver.MD5, $driver.SHA1, $driver.SHA256))
+        if ($tbs.Length -eq 96){
+            # This entry has a SHA384 TBS so we can't know for sure if this driver matches
+            if (($file_attrib) -and ($blocked_files.RuleID -contains $file_attrib.ID)){
+                $maybe_allowed.Add(("MD5:{0} SHA1:{1} SHA256:{2}" -f $driver.MD5, $driver.SHA1, $driver.SHA256))
+            }
+        }
     }
     return $false
 }
 
 foreach ($driver in $loldrivers.KnownVulnerableSamples) {
     if(hasBlockedHash $driver){
+        $not_allowed.Add(("MD5:{0} SHA1:{1} SHA256:{2}" -f $driver.MD5, $driver.SHA1, $driver.SHA256)) | Out-Null
         continue
     }
 
     $file_max_version = ($file_rules.Deny | Where-Object {$_.FileName -eq $driver.OriginalFilename}).MaximumFileVersion
     $version = (-split ($driver.FileVersion -replace ',\s*', '.'))[0]
     if($file_max_version -and $version -and ([version]$version -le $file_max_version)){
+        $not_allowed.Add(("MD5:{0} SHA1:{1} SHA256:{2}" -f $driver.MD5, $driver.SHA1, $driver.SHA256)) | Out-Null
         continue
     }
 
     if(hasBlockedSigner $driver){
+        $not_allowed.Add(("MD5:{0} SHA1:{1} SHA256:{2}" -f $driver.MD5, $driver.SHA1, $driver.SHA256)) | Out-Null
         continue
     }
 
     $allowed.Add(("MD5:{0} SHA1:{1} SHA256:{2}" -f $driver.MD5, $driver.SHA1, $driver.SHA256)) | Out-Null
 }
 
-Write-Output "Allowed:"
+Write-Output ("Number of blocked drivers: {0}" -f $not_allowed.Count)
+Write-Output ("Number of allowed drivers: {0}`n" -f $allowed.Count)
+Write-Output "Allowed drivers:"
 $allowed | ForEach-Object {Write-Output "$_"}
-Write-Output "`nMaybe allowed:"
-$maybe_allowed | ForEach-Object {Write-Output "$_"}
+if ($maybe_allowed.Count -ne 0){
+    Write-Output "`nMaybe allowed:"
+    $maybe_allowed | ForEach-Object {Write-Output "$_"}
+}
